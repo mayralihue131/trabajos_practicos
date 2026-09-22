dc11fa02d055194553805eaf48c745a22a767ce4
Mayra Aucapiña <mayralihue131@gmail.com>
2026-09-22 17:13:02 -0300

    Agregar titulo principal

---
+++ Memotest/archivosC/main.c
@@ -0,0 +1,17 @@
+/7
+pack a elegir ??
+
+public class hola{
+
+    public static void main(String[] arg){
+
+        System.out.println("Hola mundo ");
+    }
+
+
+}
+//a partir del siguiente tp nose que 
+//es un lenguaje a interpretar (interpretado corre linea por linea,
+// interpreta linea por linea, de acuerdo a la linea que va ejecutando va a cambiando al interpretacion del codigo)
+//lo miramos en tiempo de ejecucion
+//ventajas: es ser un lenguaje compilado
